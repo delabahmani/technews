cb02b47fde6c5ad721ffd7a62132a53dc8d8a3a5
delabahmani <delabahmani@gmail.com>
2024-07-19 12:39:58 -0400

    delete post functional

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,7 +1,32 @@
-export default function DeleteButton() {
+"use client"
+
+export default function DeleteButton({ id }: { id: string }) {
+  const handleDelete = async () => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this post?"
+    );
+
+    if (confirmed) {
+      try {
+        const res = await fetch(`/api/posts/${id}`, {
+          method: "Delete",
+          headers: {
+            "Content-type": "application/json",
+          },
+        });
+
+        if (res.ok) {
+          console.log("Post deleted!");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
-    <button className="text-red-600">
+    <button className="text-red-600" onClick={handleDelete}>
       Delete
     </button>
-  )
+  );
 }
