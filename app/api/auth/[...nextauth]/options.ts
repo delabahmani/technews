02854f47edbfee78a9454699f798f83deb1baeb4
delabahmani <delabahmani@gmail.com>
2024-07-22 16:09:12 -0400

    authOptions error

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,25 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/lib/prismadb";
+
+const customPrismaAdapter = PrismaAdapter(prisma) as any;
+
+export const authOptions: NextAuthOptions = {
+  adapter: customPrismaAdapter,
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  pages: {
+    signIn: '/sign-in'
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
